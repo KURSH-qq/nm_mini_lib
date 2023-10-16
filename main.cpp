92f74ddcea826f8827a92a793ab0b00f2bd8c454
KURSH-qq <kursh.and@mail.ru>
2023-10-17 00:35:54 +0300

    fix rk2

--- main.cpp
+++ main.cpp
@@ -58,12 +58,12 @@ const double p1 = 1, p2 = 1;
 
 int main()
 {
-    std::vector<double> ic = { 0,4 };
-    DEsolution sol1(1, 0.0001, 2, 200, 30, ic, 0.001, right_part, rk2, sd);
-    DEsolution sol2(1, 0.0001, 2, 200, 30, ic, 0.001, f3, rk2);
+    std::vector<double> ic = { 0,-1,1 };
+    DEsolution sol1(1, 0.0001, 4, 200, 30, ic, 0.001, right_part, rk4, sd);
+    DEsolution sol2(1, 0.0001, 4, 200, 30, ic, 0.001, f3, rk4);
 
     std::vector<double> ic2 = { 0,4, 1 };
-    SDEsolution sol3(1, 0.0001, 2, 200, 30, ic2, 0.001, f1, f2, rk2, 1, 1);
+    SDEsolution sol3(1, 0.0001, 2, 200, 30, ic2, -0.001, f1, f2, rk2, 1, 1);
 
 
 
--- methoods.h
+++ methoods.h
@@ -17,7 +17,16 @@ double rk2(double(*rp)(double, double), double h, double preX, double preV1) {
 double rk2(double(*f1)(double, double, double, double, double), double(*f2)(double, double, double, double, double),
     double h, double preX, double preV1, double preV2, double p1, double p2) {
     double f = f1(preX, preV1, preV2, p1, p2);
-    return preV1 + 0.5 * h * (f+ f1(preX+h,preV1+h*f,preV2+h*f2(preX,preV1,preV2,p1,p2),p1,p2));
+    return preV1 + 0.5 * h * (f + f1(preX + h, preV1 + h * f, preV2 + h * f2(preX, preV2, preV1, p1, p2), p1, p2));
+}
+
+double rk4(double(*rp)(double, double), double h, double preX, double preV1) {
+    double k1, k2, k3, k4;
+    k1 = rp(preX, preV1);
+    k2 = rp(preX + 0.5 * h, preV1 + 0.5 * h * k1);
+    k3 = rp(preX + 0.5 * h, preV1 + 0.5 * h * k2);
+    k4 = rp(preX + h, preV1 +  h * k3);
+    return preV1 +  h * (k1 + 2 * k2 + 2 * k3 + k4)/6;
 }
 
 double rk4(double(*f1)(double, double, double, double, double), double(*f2)(double, double, double, double, double),
@@ -25,12 +34,12 @@ double rk4(double(*f1)(double, double, double, double, double), double(*f2)(doub
     double k11, k12, k13, k14;
     double k21, k22, k23, k24;
     k11 = f1(preX, preV1, preV2, p1, p2);
-    k21 = f2(preX, preV1, preV2, p1, p2);
-    k12 = f1(preX + 0.5 * h, preV1 + 0.5 * h * k11, preV2 + 0.5 * k21,p1,p2);
-    k22 = f2(preX + 0.5 * h, preV1 + 0.5 * h * k11, preV2 + 0.5 * k21, p1, p2);
+    k21 = f2(preX, preV2, preV1, p1, p2);
+    k12 = f1(preX + 0.5 * h, preV1 + 0.5 * h * k11, preV2 + 0.5 * k21, p1, p2);
+    k22 = f2(preX + 0.5 * h, preV2 + 0.5 * k21, preV1 + 0.5 * h * k11, p1, p2);
     k13 = f1(preX + 0.5 * h, preV1 + 0.5 * h * k12, preV2 + 0.5 * k22, p1, p2);
-    k23 = f2(preX + 0.5 * h, preV1 + 0.5 * h * k12, preV2 + 0.5 * k22, p1, p2);
-    k14 = f1(preX + h, preV1 + h * k13, preV2 +  k23, p1, p2);
-    k24 = f2(preX + h, preV1 + h * k13, preV2 + k23, p1, p2);
-    return preV1 + (1/6)*h * (k11 + 2 * k12 + 2 * k13 + k14);
+    k23 = f2(preX + 0.5 * h, preV2 + 0.5 * k22, preV1 + 0.5 * h * k12, p1, p2);
+    k14 = f1(preX + h, preV1 + h * k13, preV2 + k23, p1, p2);
+    k24 = f2(preX + h, preV2 + k23, preV1 + h * k13, p1, p2);
+    return preV1 + h * (k11 + 2 * k12 + 2 * k13 + k14)/6;
 }
